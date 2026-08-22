07f86b759b7f74c7630c7d9cef623b89ec047b98
Thịnh Nguyễn <nguyenhungthinh2k4@gmail.com>
2026-08-22 14:27:23 +0700

    buoi1

---
+++ JavaCore/buoi1/src/Exam.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+import java.util.Date;
+public class Exam {
+    int examId;
+    String code;
+    String title;
+    CategoryQuestion categoryId;
+    LocalDate duration;
+    Account creatorId;
+    Date createDate;
+}
---
+++ JavaCore/buoi1/src/Question.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class Question {
+    int questionId;
+    String content;
+    CategoryQuestion categoryId;
+    TypeQuestion typeId;
+    Account creatorId;
+    Date createDate;
+}
